ccb00500ef8b262950c5357205ca523b80c48862
konchitha <konchitha@gmail.com>
2016-12-04 11:50:21 +0530

    Welcome page is created

---
+++ src/com/practice/spring/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package com.practice.spring.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends  AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		ModelAndView modelAndView =new ModelAndView("HelloPage");
+		modelAndView.addObject("welcome","Hi students");
+		return modelAndView;
+	}
+
+}
