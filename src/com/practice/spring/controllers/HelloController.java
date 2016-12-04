7c8c353d797643a42651ebc6bdbe33078e29106e
konchitha <konchitha@gmail.com>
2016-12-04 13:55:41 +0530

    multi action controller added

--- src/com/practice/spring/controllers/HelloController.java
+++ src/com/practice/spring/controllers/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 @Controller
+@RequestMapping("/greet")
 
 public class HelloController {
 
@@ -17,5 +18,12 @@ public class HelloController {
 		modelAndView.addObject("welcome","Hi students welcome");
 		return modelAndView;
 	}
+	@RequestMapping("/hi")
+	public ModelAndView  Hiworld(){
+		ModelAndView modelAndView =new ModelAndView("HelloPage");
+		modelAndView.addObject("welcome","Hi ");
+		return modelAndView;
+	}
+	
 
 }
